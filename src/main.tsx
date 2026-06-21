ef3ab3bafb10ccc3377fb0fef96f06519677595a
shiyaxiang58-netizen <shiyaxiang58@gmail.com>
2026-06-22 04:46:48 +0800

    ?? src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import { AppStoreProvider } from './store/AppStore'
+import { App } from './App'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><HashRouter><AppStoreProvider><App /></AppStoreProvider></HashRouter></React.StrictMode>,
+)
